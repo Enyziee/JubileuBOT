8c6dbbc245ac3c9e8a2531aa99914e158a30a2f5
Enyziee <kenedysamuel63@gmail.com>
2022-12-08 16:03:12 -0300

    Player reescrito com algumas melhorias

--- src/modules/MusicPlayer.js
+++ src/modules/MusicPlayer.js
@@ -1,4 +1,4 @@
-const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource, NoSubscriberBehavior, AudioPlayerStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, getVoiceConnection, VoiceConnectionStatus, entersState, createAudioPlayer, NoSubscriberBehavior, createAudioResource } = require('@discordjs/voice');
 const play = require('play-dl');
 const Queue = require('../modules/Queue');
 
@@ -7,141 +7,114 @@ class MusicPlayer {
         this.guildId = guildId;
         this.voiceAdapterCreator = voiceAdapterCreator;
 
-        this.isPlaying = false;
-        this.audioPlayer = undefined;
+        this.playing = false;
+        this.player = null;
 
         this.playlist = new Queue();
     }
 
     /**
-     * It joins a voice channel, and if it doesn't exist, it creates a new one
-     * @param channelId - The ID of the voice channel you want to join.
+     * It joins a voice channel and if it disconnects, it tries to reconnect to the same channel
+     * @param channelId - The ID of the voice channel to join
      */
-    joinVoiceChannel(channelId) {
+    async joinVC(channelId) {
 
-        // TO-DO Criar linstener para verificar erros na conexão de voz
+        let connection = getVoiceConnection(this.guildId);
 
-        if (getVoiceConnection(this.guildId) == undefined) {
-            joinVoiceChannel({
+        if (connection == undefined) {
+            connection = joinVoiceChannel({
                 channelId: channelId,
                 guildId: this.guildId,
                 adapterCreator: this.voiceAdapterCreator,
-                selfDeaf: 'false',
-            });
-        }
-    }
-
-    /**
-     * It gets the next video in the queue, creates a stream from it, and then creates an audio
-     * resource from that stream
-     * @returns A resource object.
-     */
-    async getResource() {
-        const videoInfo = this.playlist.dequeue();
-        const stream = await play.stream_from_info(videoInfo);
-        const resource = createAudioResource(stream.stream, {
-            inputType: stream.type,
-        });
-
-        return resource;
-    }
-
-    /**
-     * It creates an audio player, subscribes it to the voice connection, plays the resource, and then
-     * when the resource is finished playing, it checks if the playlist is empty. If it's not empty, it
-     * plays the next resource
-     */
-    async play(query) {
-
-        const voiceConnection = getVoiceConnection(this.guildId);
-
-        if (this.isPlaying == true) {
-
-            const videoInfo = await play.video_info(query);
-
-            this.playlist.enqueue(videoInfo);
-            return;
-        }
-
-        if (this.audioPlayer == null) {
-            this.audioPlayer = createAudioPlayer({
-                behaviors: {
-                    noSubscriber: NoSubscriberBehavior.Pause,
-                },
+                // debug: true,
             });
         }
 
-        let resource;
-
-        voiceConnection.subscribe(this.audioPlayer);
-
-        if (this.playlist.size() < 1) {
-            const stream = await play.stream(query);
-            resource = createAudioResource(stream.stream, {
-                inputType: stream.type,
-            });
-            this.audioPlayer.play(resource);
-            this.isPlaying = true;
-
-        } else {
-            this.audioPlayer.play(await this.getResource());
-            this.isPlaying = true;
-        }
-
-        this.setLinstener();
-
-    }
-
-    setLinstener() {
-        this.audioPlayer.on('stateChange', (oldState, newState) => {
-            if (oldState.status == AudioPlayerStatus.Playing && newState.status == AudioPlayerStatus.Idle) {
-                console.log('A música acabou');
-
-                this.isPlaying = false;
-
-                if (!this.playlist.isEmpty()) {
-                    this.getResource().then(resource => {
-                        this.audioPlayer.play(resource);
-                        this.isPlaying = true;
-                    });
-                }
-
+        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+            try {
+                await Promise.race([
+                    entersState(connection, VoiceConnectionStatus.Signalling, 5_00),
+                    entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+                ]);
+                // Seems to be reconnecting to a new channel - ignore disconnect
+            } catch (error) {
+                // Seems to be a real disconnect which SHOULDN'T be recovered from
+                connection.destroy();
             }
         });
+
+        connection.on('error', error => {
+            console.error(error);
+            connection.destroy();
+        });
+
+        connection.on('debug', msg => {
+            console.log(`[DEBUG] ${msg}`);
+        });
     }
 
-    nextSong() {
-        const song = this.playlist.dequeue;
-        
+    preparePlayer() {
+        const connection = getVoiceConnection(this.guildId);
+        this.player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Pause,
+            },
+        });
 
-
-        return song
+        connection.subscribe(this.player);
     }
 
-    async addSong(videoInfo) {
+    async playNow(query) {
+        this.preparePlayer();
 
-        this.playlist.enqueue(videoInfo);
-        console.log();
+        const source = await play.stream(query);
+
+        const resource = createAudioResource(source.stream, {
+            inputType: source.type,
+        });
+
+        this.player.play(resource);
+        this.playing = true;
+
+        const info = await play.video_basic_info(query);
+        return info;
+    }
+
+    async playNext() {
+        const data = await this.getNextSong();
+        this.player.play(data[1]);
+
+        return data[0];
+    }
+
+    async getNextSong() {
+        const info = this.playlist.dequeue();
+        const source = await play.stream_from_info(info);
+        const resource = createAudioResource(source.stream, {
+            inputType: source.type,
+        });
+
+        const data = [source, resource];
+
+        return data;
+    }
+
+    getQueue() {
+        // TO DO
+    }
+
+    async addSong(query) {
+        const info = await play.video_basic_info(query);
+        this.playlist.enqueue(info);
+
+        return info;
+    }
+
+    destroy() {
+        const connection = getVoiceConnection(this.guildId);
+        connection.destroy();
     }
 
 }
 
-/**
- * If the music player for the guild doesn't exist, create it and add it to the client's music players
- * @param {Client} client - The Discord.js client object.
- * @param {String} guildId - The ID of the guild the music player is for.
- * @param {voiceAdapterCreator} voiceAdapterCreator
- * @returns {MusicPlayer} The music player for the guild.
- */
-
-function getMusicPlayer(client, guildId, voiceAdapterCreator) {
-    if (client.musicPlayers.get(guildId) == undefined) {
-        const musicPlayer = new MusicPlayer(guildId, voiceAdapterCreator);
-        client.musicPlayers.set(guildId, musicPlayer);
-    }
-
-    return client.musicPlayers.get(guildId);
-
-}
-
-module.exports = { MusicPlayer, getMusicPlayer };
+module.exports = { MusicPlayer };
