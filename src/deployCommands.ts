870ad7aa3e6e2eef2b14548eb42434b71ff0c960
enyziee <kenedysamuel63@gmail.com>
2023-07-19 15:09:47 -0300

    Script para realização do deploy dos comandos

---
+++ src/deployCommands.ts
@@ -0,0 +1,38 @@
+import { REST, Routes } from "discord.js";
+import { readdirSync } from "fs";
+import path from "node:path";
+import 'dotenv/config';
+import { CommandType } from "./types/Command";
+
+const commandPath = path.join(__dirname, 'commands');
+const slashCommands: CommandType[] = [];
+const commandsNames = readdirSync(commandPath).filter((file: string) => file.endsWith('.js'));
+const rest = new REST().setToken(process.env.token!);
+
+
+Promise.all(commandsNames.map((fileName) => {
+    return import(`${commandPath}/${fileName}`);
+})).then((resolvedCommands => {
+    resolvedCommands.forEach((resCommand => {
+        slashCommands.push(resCommand.default.default);
+    }));
+
+    console.log(slashCommands);
+    postCommands(slashCommands);
+
+})).catch(err => {
+    console.log(err);
+});
+
+
+function postCommands(slashCommands: CommandType[]) {
+    console.log(`Started refreshing ${slashCommands.length} application (/) commands.`);
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = rest.put(
+        Routes.applicationGuildCommands(process.env.clientid!, process.env.guildid!),
+        { body: slashCommands },
+    ).then((data) => { return data; }).catch(err => {
+        console.log(err);
+    });
+}
